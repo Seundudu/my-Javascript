334984177ff9f24e08f8bed12f2fa6c6c51fc685
Oluwaseun Oresanwo <arewadudu@gmail.com>
2022-06-23 23:58:55 +0100

    *repo*

---
+++ myself.js
@@ -0,0 +1,14 @@
+ alert("Hello World")
+ alert ("My name is Oluwaseun Oresanwo")
+ alert ("My height is 1.7m")
+ alert("My country is Nigeria")
+
+ let myFirstName = "Oluwaseun"
+ let myLastName = "Oresanwo" 
+ let myName = "My name is Oluwaseun Oresanwo"
+ let myHeight = "My height is 1.7m"
+ let myCountry = "My country is Nigeria"
+ 
+ console.log(myName)
+ console.log(myHeight)
+ console.log(myCountry)
